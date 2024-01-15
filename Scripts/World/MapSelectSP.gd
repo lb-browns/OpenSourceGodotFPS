466eb1a61a15fd19fd552a24aad3fabf8a88ad39
lbbrowns <manysmokingmonkeys@gmail.com>
2024-01-14 21:34:12 -0800

    Updated Main Menu UI

--- Scripts/World/MapSelectSP.gd
+++ Scripts/World/MapSelectSP.gd
@@ -1,6 +1,7 @@
 extends Node
 @onready var Tutorial = load("res://tscn/World/Maps/tutorial.tscn")
 @onready var newRun = load("res://tscn/World/SHRooms/RoomGenTest.tscn")
+@onready var MainMenu = load("res://tscn/World/Maps/mainMenu.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -12,9 +13,13 @@ func _process(delta):
 	pass
 
 
-func _on_texture_button_pressed():
+func _on_tutorial_pressed():
 	get_tree().change_scene_to_packed(Tutorial)
 
 
-func _on_texture_button_2_pressed():
+func _on_new_r_un_pressed():
 	get_tree().change_scene_to_packed(newRun)
+
+
+func _on_back_pressed():
+	get_tree().change_scene_to_packed(MainMenu)
